8c02fd4e2226ebe95d39088cf585ae72786075f6
ManNjoro <elijohnmwoho@gmail.com>
2024-02-22 01:24:25 +0300

    Adding Views

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,66 @@
+﻿using CompanyManagementSystem.Data;
+using CompanyManagementSystem.Models;
+using CompanyManagementSystem.Views.Shared.Components.SearchBar;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CompanyManagementSystem.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public EmployeeController(ApplicationDbContext db)
+        {
+                _db = db;
+        }
+
+        private List<SelectListItem> GetPageSizes(int selectedPageSize = 10)
+        {
+            var pagesSizes = new List<SelectListItem>();
+            if (selectedPageSize == 5)
+                pagesSizes.Add(new SelectListItem("5", "5", true));
+            else
+                pagesSizes.Add(new SelectListItem("5", "5"));
+
+            for (int lp = 10; lp <= 100; lp += 10)
+            {
+                if (lp == selectedPageSize)
+                {
+                    pagesSizes.Add(new SelectListItem(lp.ToString(), lp.ToString(), true));
+                }
+                else
+                    pagesSizes.Add(new SelectListItem(lp.ToString(), lp.ToString()));
+            }
+            return pagesSizes;
+        }
+
+        public IActionResult Index(int pg = 1, string SearchText = "", int pageSize = 5)
+        {
+            List<Employee> employees;
+            if (SearchText != "" && SearchText != null)
+            {
+                employees = _db.Employees
+                    .Where(cat => cat.FirstName.Contains(SearchText))
+                    .ToList();
+            }
+            else
+                employees = _db.Employees.OrderByDescending(employee => employee.UpdatedAt).ToList();
+
+            if (pg < 1) pg = 1;
+            int recsCount = employees.Count();
+            var pager = new Pager(recsCount, pg, pageSize);
+            int recSkip = (pg - 1) * pageSize;
+            var data = employees.Skip(recSkip).Take(pager.PageSize).ToList();
+            SPager SearchPager = new SPager(recsCount, pg, pageSize) { Action = "index", Controller = "employee", SearchText = SearchText };
+            ViewBag.SearchPager = SearchPager;
+            this.ViewBag.PageSizes = GetPageSizes(pageSize);
+            return View(data);
+        }
+
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            return View();
+        }
+    }
+}
---
+++ Views/Shared/Components/SearchBar/SearchBarViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyManagementSystem.Views.Shared.Components.SearchBar
+{
+    public class SearchBarViewComponent : ViewComponent
+    {
+        public SearchBarViewComponent() { }
+
+        public IViewComponentResult Invoke(SPager SearchPager, bool BottomBar = false)
+        {
+            if (BottomBar)
+            {
+                return View("BottomBar", SearchPager);
+            }
+            return View("Default", SearchPager);
+        }
+    }
+}
