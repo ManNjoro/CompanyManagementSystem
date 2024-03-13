3718dd387bc04512dd0eb70a13264746576ec5a4
Man Njoro <elijohnmwoho@gmail.com>
2024-03-13 16:16:48 +0300

    Created Reports folder

--- Controllers/ReportsController.cs
+++ Controllers/ReportsController.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCore.Reporting;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CompanyManagementSystem.Controllers
 {
     public class ReportsController : Controller
     {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ReportsController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
         public IActionResult Index()
         {
             return View();
@@ -11,6 +17,14 @@ namespace CompanyManagementSystem.Controllers
 
         public IActionResult Print()
         {
+            /*string mimtype = "";
+            int extension = 1;
+            var path = $"{_webHostEnvironment.WebRootPath}\\Reports\\Report1.rdlc";
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("rp1", "Welcome to Dunder Mifflin");
+            LocalReport localReport = new LocalReport(path);
+            var result = localReport.Execute(RenderType.Pdf, extension, parameters, mimtype);
+            return File(result.MainStream, "application/pdf");*/
             return View();
         }
     }
