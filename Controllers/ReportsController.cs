fbb6efd7af0b245639ad5f6ebe2bfc65f08c45da
Man Njoro <elijohnmwoho@gmail.com>
2024-03-13 10:00:20 +0300

    Reports viewer

---
+++ Controllers/ReportsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyManagementSystem.Controllers
+{
+    public class ReportsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Print()
+        {
+            return View();
+        }
+    }
+}
